d35803f542a83ceb7a0e617d6e4e2fcddc723257
SHINYA <shinya.s.jpn@gmail.com>
2024-03-22 12:58:08 +0900

    Create t_bedrock.sql
    Sample Table t_bedrock

---
+++ t_bedrock.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `t_bedrock` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `country` varchar(52) NOT NULL DEFAULT '',
+  `information` varchar(2048),
+  `modify_user` varchar(255) NOT NULL DEFAULT (current_user()),
+  `updated_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `idx_update_time` (`updated_time`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
