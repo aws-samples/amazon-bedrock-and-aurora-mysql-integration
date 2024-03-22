16f560a8ec1d2ea1064e8438be208637a2bc5e83
SHINYA <shinya.s.jpn@gmail.com>
2024-03-22 12:57:01 +0900

    Create t_feed.sql
    sample table t_feed

---
+++ t_feed.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `t_feed` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(1024) DEFAULT NULL,
+  `link` varchar(2048) DEFAULT NULL,
+  `product` varchar(512) DEFAULT NULL,
+  `description` text,
+  `summary` text,
+  `modify_user` varchar(255) DEFAULT (current_user()),
+  `updated_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `idx_update_time` (`updated_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
