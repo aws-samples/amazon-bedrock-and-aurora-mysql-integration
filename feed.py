6854fc4d399b6b69a9ff10ddc708c51a133f3e63
SHINYA <shinya.s.jpn@gmail.com>
2024-03-22 13:11:10 +0900

    get rss data from whats-new

---
+++ feed.py
@@ -0,0 +1,27 @@
+import pymysql
+import feedparser
+import re
+
+# if module is not installed, please install it. ex: pip install -r requirements.txt
+
+myfeed = feedparser.parse("https://aws.amazon.com/about-aws/whats-new/recent/feed/")
+db = pymysql.connect(
+        host='<Aurora MySQL Writer Endpoint>',
+        user='<user name>',
+        password='<password>',
+        database='<sample schema (ex: bedrockdb)>',
+        cursorclass=pymysql.cursors.DictCursor)
+
+with db:
+  with db.cursor() as cur:
+    for item in myfeed['items']:
+        title = item.title
+        link = item.link 
+        description =  re.sub(r'<[^>]+>', '', item.description).replace("'", "\\'").replace('"', '\\"').replace('\n', ' ')
+        print (title)
+        print (link)
+        print (description)
+
+        cur.execute("INSERT INTO t_feed (title, link, description) VALUES (%s, %s, %s)", (title, link, description))
+  db.commit()
+  print ('Import rss Succesfull!')
