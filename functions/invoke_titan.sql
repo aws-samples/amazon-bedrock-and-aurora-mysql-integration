46dd575e126ac7cf555d86a43b2e3116b8994deb
SHINYA <shinya.s.jpn@gmail.com>
2024-03-29 10:35:40 +0900

    Update invoke_titan.sql
    update comment

--- functions/invoke_titan.sql
+++ functions/invoke_titan.sql
@@ -1,6 +1,6 @@
 CREATE FUNCTION invoke_titan (request_body TEXT)
 RETURNS TEXT
 ALIAS AWS_BEDROCK_INVOKE_MODEL
-MODEL ID 'amazon.titan-text-express-v1' /*** model ID ***/
+MODEL ID 'amazon.titan-text-express-v1'
 CONTENT_TYPE 'application/json'
 ACCEPT 'application/json';
