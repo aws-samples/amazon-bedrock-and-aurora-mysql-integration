4e7e7ea52ec3e04a764e309faa7560fbff580809
SHINYA <shinya.s.jpn@gmail.com>
2024-05-13 16:43:37 +0900

    Limit length of prepared statement.
    If the data size is small, there is no particular need to limit it. However, if there is a large amount of data, it is limited because PREPARED STATEMENTS can cause errors.

--- dml/summarize_ten_articles_together.sql
+++ dml/summarize_ten_articles_together.sql
@@ -3,7 +3,9 @@
 set session group_concat_max_len = 1048576; 
 set session aurora_ml_inference_timeout = 30000;
 
+-- If the data size is small, there is no particular need to limit it. However, if there is a large amount of data, it is limited because PREPARED STATEMENTS can cause errors.
 -- set @all = (select group_concat(description) from t_feed);
+
 set @all = (select group_concat(top50.description) from (select description from t_feed limit 50) top50);
 set @question = concat('\"messages\": [{\"role\": \"user\",\"content\": [{\"type\": \"text\", \"text\": \"Please categorize and tell me what kind of services improvement being talked about based on the following content.  ', @all,' ?\"}]}]}\'),\"$.content[0].text\")) as response_from_bedrock');
 set @parameter = '(\'{\"anthropic_version\": \"bedrock-2023-05-31\",\"max_tokens\": 1024,\"temperature\": 0,\"top_p\": 0, \"top_k\":1, \"stop_sequences\": [],';
